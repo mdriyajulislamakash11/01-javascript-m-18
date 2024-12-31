9197889a2e68d106fcee3d0db186a2c1b65f8d45
akash ahmed <hafejmohammadakash@gmail..com>
2025-01-01 02:32:11 +0600

    second commit

---
+++ task-1.js
@@ -0,0 +1,13 @@
+/***
+Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+*/
+
+const burger = 500;
+if(burger >= 500){
+      console.log("cock free")
+}
+else{
+      console.log("30 tk cock fee")
+}
