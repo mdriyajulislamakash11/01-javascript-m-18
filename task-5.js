14b8364836e35375c09514a867757701d3b7b385
akash ahmed <hafejmohammadakash@gmail..com>
2025-01-01 11:51:41 +0600

    ternary oparetor

---
+++ task-5.js
@@ -0,0 +1,28 @@
+/***
+
+you have two numbers in two variables, called: num1, num2
+
+now declare a variable called result. 
+if num1 is bigger than num2 then result will be double of num1. if not, then the value of the variable result will be the sum of num1 and num2.
+
+write a simple if-else. 
+
+also, write it using ternary operator.
+
+ */
+
+let num1 = 7;
+let num2 = 6;
+num1 > num2 ? console.log(num1 * 2 ): console.log(num1 + num2);
+
+
+// let num1 = 3;
+// let num2 = 6;
+
+// if(num1 > num2){
+//       const multiplay = num1 * 2;
+//       console.log("the result is =", multiplay); 
+// }else{
+//       const result = num1 + num2;
+//       console.log(result);
+// }
